cd1605b78858838043ff9bf6c7de25eede18d09d
Marcus Vinícius <mv.cunha2009@gmail.com>
2021-06-26 16:01:06 -0300

    Traduz o arquivo config.js do VuePress.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,20 +3,8 @@ const {
 } = require('../package')
 
 module.exports = {
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#title
-     */
     title: 'GPU Buyers Guide',
-    /**
-     * Ref：https://v1.vuepress.vuejs.org/config/#description
-     */
     description: description,
-
-    /**
-     * Extra tags to be injected to the page HTML `<head>`
-     *
-     * ref：https://v1.vuepress.vuejs.org/config/#head
-     */
     head: [
         ['meta', {
             name: 'theme-color',
@@ -36,13 +24,6 @@ module.exports = {
         },]
     ],
     base: '/GPU-Buyers-Guide/',
-
-
-    /**
-     * Theme configuration, here is the default theme configuration for VuePress.
-     *
-     * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-     */
     theme: 'vuepress-theme-succinct',
     globalUIComponents: [
         'ThemeManager'
@@ -50,49 +31,43 @@ module.exports = {
 
     themeConfig: {
         lastUpdated: true,
-        repo: 'https://github.com/dortania/GPU-Buyers-Guide',
+        repo: 'https://github.com/deomkds/GPU-Buyers-Guide',
         editLinks: false,
         docsDir: '',
         editLinkText: '',
         logo: '/homepage.png',
         nav: [{
-            text: 'Dortania Guides',
+            text: 'Outros Guias',
             ariaLabel: 'Language Menu',
             items: [{
-                text: 'Home Site',
+                text: 'Site Original (em inglês)',
                 link: 'https://dortania.github.io/'
             },
             {
-                text: 'Getting Started With ACPI',
-                link: 'https://dortania.github.io/Getting-Started-With-ACPI/'
+                text: 'Primeiros Passos com a ACPI',
+                link: 'https://deomkds.github.io/Getting-Started-With-ACPI/'
             },
             {
-                text: 'OpenCore Install Guide',
-                link: 'https://dortania.github.io/OpenCore-Install-Guide/'
+                text: 'Guia de Instalação do OpenCore',
+                link: 'https://deomkds.github.io/OpenCore-Install-Guide/'
             },
             {
-                text: 'OpenCore Post-Install',
-                link: 'https://dortania.github.io/OpenCore-Post-Install/'
+                text: 'Pós-instalação do OpenCore',
+                link: 'https://deomkds.github.io/OpenCore-Post-Install/'
             },
             {
-                text: 'Wireless Buyers Guide',
-                link: 'https://dortania.github.io/Wireless-Buyers-Guide/'
+                text: 'Guia de Compra de Wi-Fi',
+                link: 'https://deomkds.github.io/Wireless-Buyers-Guide/'
             },
             {
-                text: 'Anti Buyers Guide',
-                link: 'https://dortania.github.io/Anti-Hackintosh-Buyers-Guide/'
+                text: 'Guia de Compra Anti-Hackintosh',
+                link: 'https://deomkds.github.io/Anti-Hackintosh-Buyers-Guide/'
             },
             ]
         },
-            /*
-              {
-                text: 'Github',
-                link: 'https://github.com/dortania/OpenCore-Install-Guide'
-              }
-            */
         ],
         sidebar: [{
-            title: 'Introduction',
+            title: 'Introdução',
             collapsable: false,
             sidebarDepth: 1,
             children: [
@@ -101,27 +76,27 @@ module.exports = {
 
         },
         {
-            title: 'Modern GPUs',
+            title: 'GPUs Modernas',
             collapsable: false,
             sidebarDepth: 2,
             children: [
                 '/modern-gpus/amd-gpu',
                 '/modern-gpus/nvidia-gpu',
-				'/modern-gpus/intel-gpu',
+				        '/modern-gpus/intel-gpu',
             ]
         },
         {
-            title: 'Legacy GPUs',
+            title: 'GPUs Antigas',
             collapsable: false,
             sidebarDepth: 1,
             children: [
                 '/legacy-gpus/legacy-amd',
                 '/legacy-gpus/legacy-nvidia',
-				'/legacy-gpus/legacy-intel',
+				        '/legacy-gpus/legacy-intel',
             ]
         },
         {
-            title: 'Buyers Guide',
+            title: 'Guia de Compra',
             collapsable: false,
             sidebarDepth: 2,
             children: [
@@ -130,26 +105,23 @@ module.exports = {
             ]
         },
         {
-            title: 'Misc',
+            title: 'Outros',
             collapsable: false,
             sidebarDepth: 2,
             children: [
                 '/misc/bootflag',
                 '/misc/discrete-laptops',
-				'/misc/common-issues',
-				'/misc/kvm',
-				'misc/credit',
+				        '/misc/common-issues',
+				        '/misc/kvm',
+				        '/misc/credit',
             ]
         },
     ],
 },
-    /**
-     * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-     */
     plugins: [
         '@vuepress/plugin-back-to-top',
         'vuepress-plugin-smooth-scroll',
-		'fulltext-search',
+		    'fulltext-search',
         ['vuepress-plugin-medium-zoom',
             {
                 selector: "img",
